05f594f8f7b9c2dc0df35d26cce9f5e426d4643b
k-be <scalli-k-be@ya.ru>
2014-08-20 16:32:53 +0700

    initial commit

---
+++ ViewMovingText/FrameView.m
@@ -0,0 +1,62 @@
+//
+//  FrameView.m
+//  ViewMovingText
+//
+//  Created by Andrew Romanov on 20.08.14.
+//  Copyright (c) 2014 Andrew Romanov. All rights reserved.
+//
+
+#import "FrameView.h"
+
+
+@interface FrameView ()
+
+@property (nonatomic, strong) UIColor* borderColor;
+@property (nonatomic, strong) UIImageView* imageView;
+
+@end
+
+
+
+@implementation FrameView
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+	if (self = [super initWithCoder:aDecoder])
+	{
+		_borderColor = [UIColor redColor];
+		self.backgroundColor = [UIColor clearColor];
+		self.contentMode = UIViewContentModeRedraw;
+		
+		_imageView = [[UIImageView alloc] initWithFrame:CGRectInset(self.bounds, 13.0, 13.0)];
+		_imageView.backgroundColor = [UIColor clearColor];
+		_imageView.userInteractionEnabled = NO;
+		_imageView.autoresizingMask = (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth);
+		_imageView.contentMode = UIViewContentModeScaleAspectFill;
+		_imageView.image = [UIImage imageNamed:@"background.jpg"];
+		_imageView.clipsToBounds = YES;
+		[self addSubview:_imageView];
+	}
+	
+	return self;
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+	[super drawRect:rect];
+	
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathAddRect(path, NULL, CGRectInset(self.bounds, 10.0, 10.0));
+	
+	CGContextSetLineWidth(context, 3);
+	CGContextSetStrokeColorWithColor(context, _borderColor.CGColor);
+	CGContextAddPath(context, path);
+	CGContextStrokePath(context);
+	
+	CGPathRelease(path);
+}
+
+
+@end
--- ViewMovingText/ViewController.m
+++ ViewMovingText/ViewController.m
@@ -7,21 +7,95 @@
 //
 
 #import "ViewController.h"
+#import "FrameView.h"
+
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet FrameView* frameView;
+@property (nonatomic, strong) IBOutlet UIImageView* backgroundImageView;
+@property (nonatomic, strong) IBOutlet UISwitch* transformSwitch;
+@property (nonatomic, strong) UIPanGestureRecognizer* panRecognizer;
+@property (nonatomic) CGPoint startCenter;
+
+- (void)panRecognizerAction:(id)sender;
+- (IBAction)transformSwitchAction:(id)sender;
+
 @end
+
+
+@interface ViewController (Private)
+
+- (void)_updateTransformation;
+
+@end
+
 
 @implementation ViewController
             
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	_panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panRecognizerAction:)];
+	[self.frameView addGestureRecognizer:_panRecognizer];
+	
+	[self _updateTransformation];
+	
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+
+- (void)viewDidLayoutSubviews
+{
+	[super viewDidLayoutSubviews];
+	
+	CGAffineTransform transform = self.frameView.transform;
+	self.frameView.transform = CGAffineTransformIdentity;
+	self.frameView.frame = CGRectInset(self.view.bounds, CGRectGetWidth(self.view.bounds) / 3.0, CGRectGetHeight(self.view.bounds) / 3.0);
+	self.frameView.transform = transform;
+}
+
+
+#pragma mark Actions
+- (void)panRecognizerAction:(id)sender
+{
+	if (_panRecognizer.state == UIGestureRecognizerStateBegan)
+	{
+		_startCenter = _frameView.center;
+	}
+	else if (_panRecognizer.state == UIGestureRecognizerStateChanged)
+	{
+		CGPoint transition = [_panRecognizer translationInView:self.view];
+		CGPoint newCenter = CGPointMake(_startCenter.x + transition.x, _startCenter.y + transition.y);
+		self.frameView.center = newCenter;
+	}
+	else
+	{
+		
+	}
+}
+
+
+- (IBAction)transformSwitchAction:(id)sender
+{
+	[self _updateTransformation];
+}
+
+@end
+
+
+#pragma mark -
+@implementation ViewController (Private)
+
+- (void)_updateTransformation
+{
+	CGAffineTransform transform = CGAffineTransformIdentity;
+	if (_transformSwitch.on)
+	{
+		transform = CGAffineTransformRotate(CGAffineTransformMakeScale(2.0, 2.0), M_PI / 3.0);
+	}
+	
+	self.frameView.transform = transform;
 }
 
 @end
